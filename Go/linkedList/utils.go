48c657f55a40483be090222b05cc9820be4d0c6d
Jenkin Lee <zhongjianlee@outlook.com>
2020-03-30 00:36:50 +0800

    feat: 链表

---
+++ Go/linkedList/utils.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+// CreateNode 创建链表
+func CreateNode(node *LNode, max int) {
+	cur := node
+	for i := 1; i < max; i++ {
+		cur.Next = &LNode{}
+		cur.Next.Data = i
+		cur = cur.Next
+	}
+}
+
+// PrintNode 打印链表的方法
+func PrintNode(info string, node *LNode) {
+	fmt.Print(info)
+	for cur := node.Next; cur != nil; cur = cur.Next {
+		fmt.Print(cur.Data, " ")
+	}
+	fmt.Println()
+}
